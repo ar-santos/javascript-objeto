1b0134f74bc91b24e1db736415b3473c0ba3b2af
ArthurSantos54 <Arthursantos4367@gmail.com>
2022-04-09 17:08:54 -0300

    exercicios-objetos

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+import dados from "../data/index.js";
+
+const {dadosUsuarios, usuario} = dados
+
+console.log(dadosUsuarios)
+console.log(usuario)
+
+for (let usuario of dadosUsuarios){
+    // console.log(usuario.id)
+    // console.log(usuario.name)
+    // console.log(usuario.username)
+}
+
+
+// Criar uma função para pesquisar um usuário na lista de usuários 
+
+// Criar uma função para pesquisar o nome de usuário e imprimir no console
+
+// Criar uma função para pegar somente o dendereço do usuário
